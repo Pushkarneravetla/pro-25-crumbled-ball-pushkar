7986098d3433f9a1ef0ce9009b073af76fa8d641
Pushkarneravetla <80430879+Pushkarneravetla@users.noreply.github.com>
2021-07-17 16:30:47 +1000

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+class paper
+{
+	constructor(x,y,width,height)
+	{
+		var options = {
+			isStatic:true			
+			}
+		this.x = x;
+		this.y = y;
+		this.width = width
+		this.height = height
+		this.body=Bodies.rectangle(x, y, width, height, options);
+		this.image = loadImage("paper.png");
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.width, this.height);
+			image(this.image, 0, 0, this.width, this.height);
+			pop()
+			
+	}
+
+}
